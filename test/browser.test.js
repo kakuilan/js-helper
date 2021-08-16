2fdf7c86b686c166ec8c48d7f7d5a2be28fe631d
kakuilan <kakuilan@163.com>
2021-08-16 22:06:39 +0800

    add getQueryByName

--- test/browser.test.js
+++ test/browser.test.js
@@ -4,6 +4,7 @@
 
 import Kcons from "../lib/constant"
 import Kbrow from "../lib/browser";
+import Kvali from "../lib/validate"
 import Kdata from "../test/data"
 
 var assert = require('assert');
@@ -21,6 +22,22 @@ describe('Browser', function () {
         let chk = res1.indexOf(res2) > 1
         it('getDomain should return not empty', function () {
             assert.equal(chk, true)
+        });
+    });
+    describe('#getQueryByName', function () {
+        let res1 = Kbrow.getQueryByName('q', Kdata.url02, false)
+        it('getQueryByName(x,y,false) should return from query', function () {
+            assert.equal(res1 !== null, true)
+        });
+
+        let res2 = Kbrow.getQueryByName('q', Kdata.url01, true)
+        it('getQueryByName(x,y,true) should return from hash when query empty', function () {
+            assert.equal(res2 !== null, true)
+        });
+
+        let res3 = Kbrow.getQueryByName(null, Kdata.url03, true)
+        it('getQueryByName(null,y,true) should return object', function () {
+            assert.equal(!Kvali.isEmpty(res3), true)
         });
     });
 
--- test/data/index.js
+++ test/data/index.js
@@ -4,11 +4,15 @@ const strHello = "Hello World! 你好，世界！"
 const helloEng = "hello world!"
 const helloWidth = "ｈｅｌｌｏ　ｗｏｒｌｄ！"
 
-const url01 = "https://h5.test.com/#/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=golang&oq=golang&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5"
+const url01 = "https://h5.test.com/#/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=javascript&oq=javascript&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5"
+const url02 = "https://h5.test.com/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=javascript&oq=javascript&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5#baz=qux&n=hello"
+const url03 = "/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=javascript&oq=javascript&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5"
 
 export default {
     strHello,
     helloEng,
     helloWidth,
     url01,
+    url02,
+    url03,
 }
