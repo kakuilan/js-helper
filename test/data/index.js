69ca76d6c749a959b504266a4502ab992952af62
kakuilan <kakuilan@163.com>
2022-05-19 22:00:34 +0800

    add Kconv.ipToInt/Kconv.intToIP

--- test/data/index.js
+++ test/data/index.js
@@ -5,11 +5,14 @@ const helloEng = "hello world!"
 const helloWidth = "ｈｅｌｌｏ　ｗｏｒｌｄ！"
 const strTest = "Hello World! 你好，世界！Olá🐍 With Emojis 🐳📜"
 const intTest = 123456
+const int01 = 3232243653
 
 const url01 = "https://h5.test.com/#/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=javascript&oq=javascript&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5"
 const url02 = "https://h5.test.com/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=javascript&oq=javascript&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5#baz=qux&n=hello"
 const url03 = "/home/indexsearch?source=hp&ei=tDUwXejNGs6DoATYkqCYCA&q=javascript&oq=javascript&gs_l=psy-ab.3..35i39l2j0i67l8.1729.2695..2888...1.0..0.126.771.2j5......0....1..gws-wiz.....10..0.fFQmXkC_LcQ&ved=0ahUKEwjo9-H7jb7jAhXOAYgKHVgJCIMQ4dUDCAU&uact=5"
 const url04 = "https://tool.google.com.net/encrypt?type=4Hello World! 你好！"
+
+const ip01 = "192.168.31.197"
 
 export default {
     strHello,
@@ -17,8 +20,10 @@ export default {
     helloWidth,
     strTest,
     intTest,
+    int01,
     url01,
     url02,
     url03,
     url04,
+    ip01,
 }
