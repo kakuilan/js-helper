7306a27ddfdaadca9d338502d87134b67a7c7fd3
kakuilan <kakuilan@163.com>
2024-05-09 20:03:46 +0800

    fix convert/index.ts

--- src/lib/convert/index.ts
+++ src/lib/convert/index.ts
@@ -1,1 +1,12 @@
+import _ from 'lodash';
+
+/**
+ * 将变量转为字符串
+ * @param {any} v 变量
+ * @returns {string}
+ */
+export function toString(v: any): string {
+  return _.toString(v);
+}
+
 export default {};
