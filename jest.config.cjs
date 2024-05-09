e315dae522216b9f26fc950b03a075e8ff5db642
kakuilan <kakuilan@163.com>
2024-05-09 15:20:16 +0800

    del files

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    verbose: true,
-    testMatch: ["**/test/**/*.test.[jt]s?(x)"],
-    testEnvironment: "node",
-    transform: {
-        "^.+\\.[t|j]sx?$": "babel-jest"
-    }
-};
